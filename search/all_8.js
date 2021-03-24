3e7192de2b7a995c7d114dee9fb5f3c37eab8011
2bndy5 <2bndy5@users.noreply.github.com>
2021-03-24 01:44:18 +0000

    deploy: dcb859bc3784d730853d58689ae53d081bfcc2a8

--- search/all_8.js
+++ search/all_8.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['sethandler_15',['sethandler',['../class_r_f24_logger.html#a2cdefbca15035f78cd3f0084e00b146f',1,'RF24Logger']]],
+  ['sethandler_15',['sethandler',['../class_r_f24_logger.html#afcc9a888c32c135d9a568971778042d0',1,'RF24Logger']]],
   ['setlevel_16',['setLevel',['../class_r_f24_logger.html#a564fe5c4520d970e5690ce01078208ef',1,'RF24Logger']]],
   ['streamtype_17',['StreamType',['../group__stream_type_doc.html',1,'']]]
 ];
--- search/functions_7.js
+++ search/functions_7.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['sethandler_30',['sethandler',['../class_r_f24_logger.html#a2cdefbca15035f78cd3f0084e00b146f',1,'RF24Logger']]],
+  ['sethandler_30',['sethandler',['../class_r_f24_logger.html#afcc9a888c32c135d9a568971778042d0',1,'RF24Logger']]],
   ['setlevel_31',['setLevel',['../class_r_f24_logger.html#a564fe5c4520d970e5690ce01078208ef',1,'RF24Logger']]]
 ];
